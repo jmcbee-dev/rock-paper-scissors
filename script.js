383c22b7a8a3656256eaf7dbffb2a30ef28ee465
jmcbee-dev <mcbee@mines.edu>
2024-02-05 12:50:50 -0700

    Completed Project #3

---
+++ script.js
@@ -0,0 +1,34 @@
+function getComputerChoice(){
+   return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)]
+}
+
+function playRound(playerChoice, computerChoice){
+   let result = '';
+   let pc = playerChoice.toLocaleLowerCase();
+   let cc = computerChoice.toLocaleLowerCase();
+
+   if(pc == cc)
+      result = `Tie! You both picked ${cc}`;
+   else {
+      if( (pc == 'rock' && cc == 'paper') 
+         || (pc == 'paper' && cc == 'scissors') 
+         || (pc == 'scissors' && cc == 'rock') )
+         result = `You lose! Computer picked ${cc}`;
+      else {
+         result = `You win! Computer picked ${cc}`;
+      }
+   }
+   return result;
+}
+
+function playGame(){
+   let playerScore = 0;
+   for(let i = 0; i < 5; i++){
+      let playerChoice = prompt("Pick between rock, paper, or scissors.");
+      let result = playRound(playerChoice, getComputerChoice());
+      console.log(result);
+      if(result.includes('win'))
+         playerScore++;
+   }
+   console.log(`You won ${playerScore} out of 5 games.`);
+}
